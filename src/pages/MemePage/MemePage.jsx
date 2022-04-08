61a401907f4feef324c60e22e3ea9304c471bb0c
Diego V <egovaldel@gmail.com>
2022-04-08 12:27:59 +0200

    export meme

--- src/pages/MemePage/MemePage.jsx
+++ src/pages/MemePage/MemePage.jsx
@@ -1,29 +1,69 @@
-
+import { Row,Col } from "antd";
+import html2canvas from "html2canvas";
+import { useState } from "react"
+import {  } from "./MemePage.css";
 
 const MemePage = () => {
+    const [textTop, setTextTop] = useState('')
+    const [textBottom, setTextBottom] = useState('')
+    const [image, setImage] = useState('')
 
+    const handleInputTextTop = e => setTextTop(e.target.value)
+    const handleInputTextBottom = e => setTextBottom(e.target.value)
+    const handleImage = e => setImage(e.target.value)
+    const options = {
+        image: image,
+        width: 500,
+        height: 500,
+        backgroundColor: '#0000'
+
+    }
+    const handlesExport = e => {
+      
+        html2canvas(document.getElementById("meme"), options)
+            .then(canvas => {
+                const imgData = canvas.toDataURL("image/png")
+                //download image    
+                const link = document.createElement("a")
+                link.download = "meme.png"
+                link.href = imgData
+                link.click()
+            })
+            .catch(err => console.log(err))
+    }
 
     return ( 
-        <div>
-            <h2>Meme Generator</h2>
-            <select>
-                <option value="fire">Girl and fire</option>
-                <option value="futurama">Futurama</option>
-                <option value="history">History channel man</option>
-                <option value="matrix">Matrix</option>
-                <option value="raptor">Velociraptor</option>
-                <option value="smart">Be smart</option>
-            </select>
-            <span>Text on top:
-                <input />
-            </span>
-            <span>Text on bottom:
-                <input />
-            </span>
-            {/* <img alt={} /> */}
-            <button>Generate</button>
-
-        </div>
+        <Row justifyContent="center">
+            <Col span={24} justify="center">
+            
+                <h2>Meme Generator</h2>
+                <select onChange={handleImage}>
+                    <option value="default">Select...</option>
+                    <option value="fire">Girl and fire</option>
+                    <option value="futurama">Futurama</option>
+                    <option value="history">History channel man</option>
+                    <option value="matrix">Matrix</option>
+                    <option value="raptor">Velociraptor</option>
+                    <option value="smart">Be smart</option>
+                </select>
+                <br/>
+                <span>Text on top:
+                    <input onChange={handleInputTextTop}/>
+                </span>
+                <br/>
+                <span>Text on bottom:
+                    <input onChange={handleInputTextBottom}/>
+                </span>
+                <br/>
+                <div id="meme">
+                    <span className="textBox topText">{textTop}</span>
+                    <br/>
+                    <span className="textBox bottomText">{textBottom}</span>
+                    <img  src={`/img/${image}.jpeg`} alt={image} />
+                </div>
+                <button onClick={handlesExport}>Generate</button>
+            </Col>
+        </Row>
 
      )
 }
